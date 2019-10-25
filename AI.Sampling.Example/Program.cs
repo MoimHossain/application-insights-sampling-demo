4c235df478f52f0194816c6c2906fb177dc8ddde
Moim HA <moimhossain@gmail.com>
2019-10-25 14:45:11 +0200

    Sampling explained

--- AI.Sampling.Example/Program.cs
+++ AI.Sampling.Example/Program.cs
@@ -17,10 +17,10 @@ namespace AI.Sampling.Example
 
 
         
-// 1. Sampling ratio is determined by the number of events per second occurring in the app
-// 2. The AI SDK randomly selects requests to be sampled 
-//    when the request begins (so, it is not known whether it will fail or succeed)
-// 3. AI SDK assigns itemCount=<sampling ratio>
+        // 1. Sampling ratio is determined by the number of events per second occurring in the app
+        // 2. The AI SDK randomly selects requests to be sampled 
+        //    when the request begins (so, it is not known whether it will fail or succeed)
+        // 3. AI SDK assigns itemCount=<sampling ratio>
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
